02943a6fb69c45613596e626a1fb0d98a78588b5
Mouse <mouse0w0@qq.com>
2019-05-04 18:37:45 +0800

    Update

--- src/main/java/unknowndomain/foundation/Foundation.java
+++ src/main/java/unknowndomain/foundation/Foundation.java
@@ -1,21 +1,27 @@
 package unknowndomain.foundation;
 
+import org.slf4j.Logger;
 import unknowndomain.engine.block.Block;
+import unknowndomain.engine.block.BlockBase;
 import unknowndomain.engine.event.Listener;
-import unknowndomain.engine.event.game.GameReadyEvent;
-import unknowndomain.engine.event.registry.RegisterEvent;
+import unknowndomain.engine.event.game.GameEvent;
 import unknowndomain.engine.mod.Mod;
+import unknowndomain.engine.mod.annotation.AutoListen;
+import unknowndomain.engine.mod.annotation.AutoRegister;
+import unknowndomain.engine.mod.annotation.Inject;
 
 @Mod("foundation:1.0.0")
+@AutoListen
 public class Foundation {
 
+    @AutoRegister
+    public static Block WOOD = new BlockBase().registerName("wood");
+
+    @Inject
+    public static Logger logger;
+
     @Listener
-    public void onGameReady(GameReadyEvent event) {
+    public void onGameReady(GameEvent.Ready event) {
         System.out.println("Hello Foundation!");
-    }
-
-    @Listener
-    public void register(RegisterEvent<Block> event) {
-
     }
 }
