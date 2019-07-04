e3839f83bbc585c9f8ed393d9eadc35423a336fb
Mouse <mouse0w0@qq.com>
2019-07-04 23:43:47 +0800

    Update

--- src/main/java/unknowndomain/foundation/Foundation.java
+++ src/main/java/unknowndomain/foundation/Foundation.java
@@ -1,27 +1,19 @@
 package unknowndomain.foundation;
 
+import com.google.inject.Inject;
+import nullengine.event.Listener;
+import nullengine.event.mod.ModLifecycleEvent;
+import nullengine.mod.annotation.Mod;
 import org.slf4j.Logger;
-import unknowndomain.engine.block.Block;
-import unknowndomain.engine.block.BlockBase;
-import unknowndomain.engine.event.Listener;
-import unknowndomain.engine.event.game.GameEvent;
-import unknowndomain.engine.mod.Mod;
-import unknowndomain.engine.mod.annotation.AutoListen;
-import unknowndomain.engine.mod.annotation.AutoRegister;
-import unknowndomain.engine.mod.annotation.Inject;
 
-@Mod("foundation:1.0.0")
-@AutoListen
+@Mod(id = "foundation", version = "0.1.0-alpha", name = "Foundation")
 public class Foundation {
-
-    @AutoRegister
-    public static Block WOOD = new BlockBase().registerName("wood");
 
     @Inject
     public static Logger logger;
 
     @Listener
-    public void onGameReady(GameEvent.Ready event) {
-        System.out.println("Hello Foundation!");
+    public void onInit(ModLifecycleEvent.Initialization event) {
+        logger.info("Hello World");
     }
 }
---
+++ src/main/java/unknowndomain/foundation/init/Blocks.java
@@ -0,0 +1,10 @@
+package unknowndomain.foundation.init;
+
+import nullengine.block.BaseBlock;
+import nullengine.mod.annotation.AutoRegister;
+
+public class Blocks {
+
+    @AutoRegister
+    public static BaseBlock WOOD = (BaseBlock) new BaseBlock().registerName("wood");
+}
