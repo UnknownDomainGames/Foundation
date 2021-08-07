e87a2ebdc53a070f9ceeef6503cb25de88d96d9b
cvrunmin <cvrunmin@outlook.com>
2021-08-07 16:30:49 +0800

    fix: remove adding player on GameStartEvent.Post (handled on engine)

--- src/main/java/unknowndomain/foundation/init/GameInitializer.java
+++ src/main/java/unknowndomain/foundation/init/GameInitializer.java
@@ -1,20 +1,13 @@
 package unknowndomain.foundation.init;
 
-import engine.block.Block;
 import engine.block.state.BlockState;
-import engine.client.game.GameClient;
-import engine.client.input.controller.EntityCameraController;
-import engine.entity.CameraEntity;
 import engine.event.Listener;
 import engine.event.Order;
 import engine.event.game.GameCreateEvent;
 import engine.event.game.GameStartEvent;
 import engine.mod.annotation.AutoListen;
 import engine.mod.annotation.AutoRegister;
-import engine.player.Profile;
 import engine.world.impl.FlatWorldCreationSetting;
-
-import java.util.UUID;
 
 @AutoRegister
 @AutoListen
@@ -31,13 +24,13 @@ public class GameInitializer {
 
     @Listener(order = Order.FIRST)
     public static void onGameStarted(GameStartEvent.Post event) {
-        if (event.getGame() instanceof GameClient) {
-            var game = (GameClient) event.getGame();
-            game.getWorld("default")
-                    .map(world -> world.spawnEntity(CameraEntity.class, 0, 6, 0))
-                    .ifPresent(entity -> game.joinPlayer(new Profile(UUID.randomUUID(), "default"), entity));
-
-            game.getClientPlayer().setEntityController(new EntityCameraController());
-        }
+//        if (event.getGame() instanceof GameClient) {
+//            var game = (GameClient) event.getGame();
+//            game.getWorld("default")
+//                    .map(world -> world.spawnEntity(CameraEntity.class, 0, 6, 0))
+//                    .ifPresent(entity -> game.joinPlayer(new Profile(UUID.randomUUID(), "default"), entity));
+//
+//            game.getClientPlayer().setEntityController(new EntityCameraController());
+//        }
     }
 }
